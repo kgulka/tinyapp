f00ef194f3d87d8691954900c7d2d436b92983e5
Kent Gulka <kentgulka@modindsoft.com>
2022-02-22 00:09:28 +0000

    added shortened url page and route:

--- express_server.js
+++ express_server.js
@@ -6,7 +6,7 @@ app.set("view engine", "ejs");
 
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselags.ca",
+  "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xk": "http://www.google.com"
 };
 
@@ -20,6 +20,12 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+//shortened urls
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
+});
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 })
