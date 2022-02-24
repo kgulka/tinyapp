6fff803b4c6f24c6d6391b81f43fc38b1c2524b4
Kent Gulka <kentgulka@modindsoft.com>
2022-02-24 00:02:16 +0000

    added New users Post route

--- express_server.js
+++ express_server.js
@@ -7,6 +7,17 @@ const generateRandomString = function() {
   }
   return result;
 };
+const users = {
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"},
+  "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+};
 
 const { application, response } = require("express");
 const express = require("express");
@@ -36,8 +47,7 @@ const dbFilePath = path.join(__dirname, 'db', 'urls_db.txt');
 fs.readFile(dbFilePath, (err, fileContent) => {
   if (err) {
     //display the error
-    //response.statusCode = 500;
-    //response.write(err.message);
+    console.log("readFile Error:", err);
   } else {
     urlDatabase = JSON.parse(fileContent);
     //console.log("urlDatabase:", urlDatabase);
@@ -53,45 +63,78 @@ app.get("/urls.json", (req, res) => {
 //Show List of URLs
 app.get("/urls", (req, res) => {
   let templateVars = {};
-  if (req.cookies["username"]) {
-    templateVars = { username: req.cookies["username"], urls: urlDatabase };
+  console.log("user_id:", req.cookies["user_id"]);
+  if (req.cookies["user_id"]) {
+    templateVars = { user_id: req.cookies["user_id"], urls: urlDatabase };
   } else {
-    templateVars = { username: undefined, urls: urlDatabase };
+    templateVars = { user_id: undefined, urls: urlDatabase };
   }
   res.render("urls_index", templateVars);
 });
 //Show new URL page
 app.get("/urls/new", (req, res)=> {
   let templateVars = {};
-  if (req.cookies["username"]) {
-    templateVars = { username: req.cookies["username"], urls: urlDatabase };
+  if (req.cookies["user_id"]) {
+    console.log("user_id:", req.cookies["user_id"]);
+    templateVars = { user_id: req.cookies["user_id"], urls: urlDatabase };
   } else {
-    templateVars = { username: undefined, urls: urlDatabase };
+    templateVars = { user_id: undefined, urls: urlDatabase };
   }
   res.render("urls_new", templateVars);
 });
-
 //Show shortened url
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { username: req.cookies["username"],
+  const templateVars = { user_id: req.cookies["user_id"],
     shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
 //shortened single url
 app.get("/u/:shortURL", (req, res) => {
   const longURL  = urlDatabase[req.params.shortURL];
-  //const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+
   if (longURL !== undefined) {
     res.redirect(longURL);
   } else {
     res.redirect("/urls");
   }
 });
-//shortened single url
+//register user 
 app.get("/register", (req, res) => {
-  const templateVars = { username: req.cookies["username"] };
+  const templateVars = { user_id: req.cookies["user_id"] };
   res.render("register", templateVars);
 });
+//write a new user to the list
+app.post("/register", (req, res) => {
+
+  const newUserID = generateRandomString();
+  const { email, password } = req.body;
+  for (let user in users) {
+    if (users[user].email === email) {
+      console.log('email already exists!');
+      return res.redirect("/urls");
+    }
+  }
+  if (!email || !password) {
+    console.log('invalid email or password');
+    return res.redirect("/urls");
+  }
+  users[newUserID] = { id: newUserID, email, password };
+  console.log("Users Obj:", users);
+ // urlDatabase[newShortURL] = req.body.longURL;
+  //Writefile to save the database here.
+  /*
+  fs.writeFile(dbFilePath, JSON.stringify(urlDatabase), function(err) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("The file was saved!");
+  });
+  */
+  res.cookie("user_id",users[newUserID]);
+  console.log("userid cookie set");
+  res.redirect("/urls");
+});
+
 //write a new URL to the list
 app.post("/urls", (req, res) => {
   //console.log(req.body);  // Log the POST request body to the console
@@ -131,20 +174,18 @@ app.post("/urls/:shortURL", (req, res) => {
     }
     console.log("The file was saved!");
   });
-
-  //console.log("A-shorturl:",urlDatabase[req.params.shortURL]);
   res.redirect("/urls");
 });
 // header login username
 app.post("/login", (req, res) => {
-  console.log("bodyUserN:",req.body.username);
-  res.cookie("username",req.body.username); //.send("cookie set");
+  console.log("bodyUserN:",req.body.user_id);
+  res.cookie("user_id",req.body.user_id); 
   console.log("cookie set");
   res.redirect("/urls");
 });
-// header logout username
+// header logout user_id
 app.post("/logout", (req, res) => {
-  res.clearCookie("username");
+  res.clearCookie("user_id");
   res.redirect("/urls");
 });
 
